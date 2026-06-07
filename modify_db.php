4cf04d30bc37aad6139b232db39b91200965b8d9
TungDuong-Dev <devopptungduong@gmail.com>
2026-06-07 18:44:39 +0700

    xoa file rac

--- modify_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
-
-Schema::table('system_configs', function(Blueprint $table) {
-    $table->longText('value')->nullable()->change();
-});
-echo "Done";
